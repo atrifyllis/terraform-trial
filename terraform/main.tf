3984115173ab60bbf6df4867f6f021114d3d49b8
atrifyllis <atrifyllis@gmail.com>
2021-10-13 21:59:18 +0300

    continue ecs cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-west-2"
+  region = var.aws_region
 }
 
 terraform {
@@ -102,7 +102,7 @@ resource "aws_ecs_cluster" "ecs_cluster" {
 
   default_capacity_provider_strategy {
     capacity_provider = aws_ecs_capacity_provider.prov1.name
-    weight = 1
+    weight            = 1
   }
 
   tags = {
@@ -118,6 +118,96 @@ resource "aws_ecs_capacity_provider" "prov1" {
     auto_scaling_group_arn = module.asg.autoscaling_group_arn
   }
 
+}
+
+resource "aws_ecs_task_definition" "spring-boot-task" {
+  family                = "spring-boot-task"
+  container_definitions = data.template_file.task_definition.rendered
+}
+
+resource "aws_ecs_service" "spring-boot-service" {
+  name            = "spring-boot-service"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.spring-boot-task.arn
+  desired_count   = 2
+  iam_role        = aws_iam_role.asg-iam-role.name
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.alb-target-group.id
+    container_name   = "ghost"
+    container_port   = "2368"
+  }
+
+  depends_on = [
+    aws_iam_role.asg-iam-role,
+    aws_alb_listener.front_end,
+  ]
+}
+
+# ALB
+
+resource "aws_alb_target_group" "alb-target-group" {
+  name     = "spring-boot-alb-target-group"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+}
+
+resource "aws_alb" "main" {
+  name            = "spring-boot-alb"
+  subnets         = module.vpc.public_subnets
+  security_groups = [module.alb_sg.security_group_id]
+}
+
+resource "aws_alb_listener" "front_end" {
+  load_balancer_arn = aws_alb.main.id
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_alb_target_group.alb-target-group.id
+    type             = "forward"
+  }
+}
+
+module "alb_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name = "alb_security-group"
+
+  vpc_id = module.vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      protocol    = "tcp"
+      from_port   = 80
+      to_port     = 80
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      from_port = 0
+      to_port   = 0
+      protocol  = "-1"
+
+      cidr_blocks = [
+        "0.0.0.0/0",
+      ]
+    }
+  ]
+}
+
+data "template_file" "task_definition" {
+  template = file("${path.module}/templates/task-definition.json")
+
+  vars = {
+    image_url        = "ghost:latest"
+    container_name   = "ghost"
+    log_group_region = var.aws_region
+    log_group_name   = aws_cloudwatch_log_group.app.name
+  }
 }
 
 
