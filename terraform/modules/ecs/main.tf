a1a2f3ec7e7242f112026bad291a2568d425fb3d
Alexandros Trifyllis <alexandros.trifyllis@trasys.gr>
2021-10-21 23:38:43 +0300

    drop container memory to 256M. use min max deployment healthe percent to try to kill tasks before creating new ones.

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -6,11 +6,13 @@ resource "aws_ecs_cluster" "ecs_cluster" {
 resource "aws_ecs_service" "ecs_service" {
   name = "ecs-service"
 
-  cluster         = aws_ecs_cluster.ecs_cluster.id
-  task_definition = aws_ecs_task_definition.ecs_task_definition.arn
-  desired_count   = 2
+  cluster                            = aws_ecs_cluster.ecs_cluster.id
+  task_definition                    = aws_ecs_task_definition.ecs_task_definition.arn
+  desired_count                      = 2
+  deployment_minimum_healthy_percent = 0
+  deployment_maximum_percent = 100
   # required when using load balancer! (dont use it if task has awsvpc network)
-  iam_role        = aws_iam_role.ecs_iam_role.name
+  iam_role                           = aws_iam_role.ecs_iam_role.name
 
   load_balancer {
     container_name   = local.container_name
