eaa54ff187c9fed3d1136c81be6828b8eec21da6
Alexandros Trifyllis <alexandros.trifyllis@trasys.gr>
2021-10-21 16:57:32 +0300

    show host info in endpoint

---
+++ src/main/kotlin/gr/alx/terraformtrial/AppInfo.kt
@@ -0,0 +1,4 @@
+package gr.alx.terraformtrial
+
+class AppInfo(val message: String, val hostName: String, val os: String) {
+}
--- src/main/kotlin/gr/alx/terraformtrial/TerraformTrialApplication.kt
+++ src/main/kotlin/gr/alx/terraformtrial/TerraformTrialApplication.kt
@@ -2,8 +2,10 @@ package gr.alx.terraformtrial
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.InetAddress
 
 @SpringBootApplication
 class TerraformTrialApplication
@@ -13,9 +15,28 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class TestController {
+class TestController(private val environment: Environment) {
     @GetMapping("/hello")
-    fun hello(): String {
-        return "hello"
+
+    fun hello(): AppInfo {
+        // obtain a hostname. First try to get the host name from docker container (from the "HOSTNAME" environment variable)
+        var hostName = System.getenv("HOSTNAME")
+
+        val hostAddress = InetAddress.getLoopbackAddress();
+        // get the os name
+        val os = System.getProperty("os.name")
+
+        // if the application is not running in a docker container, we can to obtain the hostname using the "java.net.InetAddress" class
+        if (hostName == null || hostName.isEmpty()) {
+            hostName = try {
+                val addr = InetAddress.getLocalHost()
+                addr.hostName
+            } catch (e: Exception) {
+                System.err.println(e)
+                "Unknown"
+            }
+        }
+
+        return AppInfo("Sample Java Spring Boot app", hostName, os)
     }
 }
