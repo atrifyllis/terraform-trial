23c035e223a91dfad34ff05cb266aee1ba1685e5
alx <otinanism@gmaiil.com>
2022-12-18 16:23:22 +0200

    Retrieve Lets encrypt url from tf variable.

--- terraform-eks/kops/tf/modules/certificate/main.tf
+++ terraform-eks/kops/tf/modules/certificate/main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 provider "acme" {
-  server_url = "https://acme-v02.api.letsencrypt.org/directory"
+  server_url = var.lets_encrypt_url
 }
 
 resource "tls_private_key" "reg_private_key" {
